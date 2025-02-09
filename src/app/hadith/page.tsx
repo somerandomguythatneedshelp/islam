4b04d59fef8952f92912bd9d210f9f6ec59bc850
Taha Akhtar <107365137+somerandomguythatneedshelp@users.noreply.github.com>
2025-02-09 16:05:21 +0000

    fixed ui on mobile

--- src/app/hadith/page.tsx
+++ src/app/hadith/page.tsx
@@ -30,9 +30,9 @@ export default function HadithPage() {
     };
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="flex flex-col space-y-4 p-5 mr-96 -ml-96">
-                <h1>Choose a Book:</h1>
+        <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-24">
+            <div className="flex flex-col space-y-4 p-5 w-full sm:w-auto">
+                <h1 className="text-center sm:text-left">Choose a Book:</h1>
                 <label className="relative flex items-center cursor-pointer">
                     <input
                         className="sr-only peer"
@@ -84,11 +84,11 @@ export default function HadithPage() {
                     <span className="ml-2 text-white">Al-Tirmidhi</span>
                 </label>
             </div>
-            <div className="mb-56 ml-24 mr-24">
+            <div className="mt-8 w-full sm:w-auto">
                 {loading && <div>Loading...</div>}
                 {error && <div>Error: {error}</div>}
                 {hadithData && (
-                    <div className="p-4 border rounded-lg">
+                    <div className="p-4 border rounded-lg mb-64">
                         <p>{hadithData.data.id}</p>
                         <p className="text-lg">{hadithData.data.hadith_english}</p>
                         <p className="text-sm text-gray-500">{hadithData.reference}</p>
